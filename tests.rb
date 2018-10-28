22f23fc99382d3afdaad9cd3fbd835cce3994ab4
Laendasill <panador@gmail.com>
2018-10-28 16:42:42 +0100

    rubocop style auto format

--- tests.rb
+++ tests.rb
@@ -4,27 +4,26 @@ require './App'
 
 Capybara.app = App
 
-describe "the index page", type: :feature do
+describe 'the index page', type: :feature do
+  it 'shows form' do
+    visit '/'
+    expect(page).to have_xpath('//form')
+  end
 
-    it "shows form" do
-      visit '/'
-      expect(page).to have_xpath('//form')
-    end
+  it 'shows error on bad form' do
+    visit '/'
+    fill_in('url_name', with: 'bad url')
+    find('button[type="submit"]').click
 
-    it "shows error on bad form" do
-        visit '/'
-        fill_in("url_name", with: "bad url")
-        find('button[type="submit"]').click
+    expect(page).to have_content("It's not a valid URL")
+  end
 
-        expect(page).to have_content("It's not a valid URL")
-    end
+  it 'shows url on good form' do
+    valid_url = 'http://valid.pl'
+    visit '/'
+    fill_in('url_name', with: valid_url)
+    find('button[type="submit"]').click
 
-    it "shows url on good form" do
-        valid_url = "http://valid.pl"
-        visit '/'
-        fill_in("url_name", with: valid_url)
-        find('button[type="submit"]').click
-
-        expect(page).to have_xpath('//a')
-    end
+    expect(page).to have_xpath('//a')
+  end
 end
